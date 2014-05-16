7b87663faee4e494c8e3cf52148a5fe1c2e0b8b6
Dévai Nándor <mail@devainandor.com>
2014-05-16 14:39:15 +0200

    added page handling

--- wiki.py
+++ wiki.py
@@ -2,6 +2,13 @@ from wsgiref.simple_server import make_server
 from string import Template as T
 import codecs
 
+empty_article = """\
+<h1 contenteditable="true">New page</h1>
+<div contenteditable="true">
+<p>…</p>
+</div>
+"""
+
 template = T("""\
 <!DOCTYPE html>
 <meta charset="utf-8">
@@ -15,30 +22,47 @@ var saveButton = document.getElementById('save');
 saveButton.addEventListener('click', function() {
     var article = document.getElementsByTagName('article')[0];
     var request = new XMLHttpRequest();
-    request.open('POST', '/');
+    request.open('POST', document.URL);
     request.send(article.innerHTML);
 });
 </script>
 """)
 
 
+def get_filename(environ):
+    pagename = environ['PATH_INFO'].strip('/')
+    if pagename == '':
+        return 'index.html'
+    else:
+        return pagename + '.html'
+
+
 def write_file(environ):
     body = environ['wsgi.input'].read(int(environ['CONTENT_LENGTH']))
-    print(body.decode('utf-8'))
-    with codecs.open('page.html', 'w', 'utf-8') as page:
+    filename = get_filename(environ)
+    with codecs.open(filename, 'w', 'utf-8') as page:
         page.write(body.decode('utf-8').strip())
 
 
-def read_file():
-    return codecs.open('page.html', 'r', 'utf-8').read()
+def read_file(environ):
+    filename = get_filename(environ)
+    try:
+        return codecs.open(filename, 'r', 'utf-8').read()
+    except IOError:
+        return empty_article
 
 
+# available URLs:
+# POST <page name>
+# GET <page name>
+# GET /
 def wiki(environ, start_response):
     if environ['REQUEST_METHOD'].upper() == 'POST':
         write_file(environ)
-    article = read_file()
+    article = read_file(environ)
     start_response('200 OK', [('Content-Type', 'text/html')])
     return [template.substitute(article=article).encode('utf-8')]
 
-srv = make_server('localhost', 5000, wiki)
-srv.serve_forever()
+if __name__ == '__main__':
+    srv = make_server('localhost', 5000, wiki)
+    srv.serve_forever()
