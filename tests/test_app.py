6f1bd6ec5898125f7052d60ea357f5974fcf8738
Dévai Nándor <mail@devainandor.com>
2014-05-20 14:12:59 +0200

    wiki shows existing and new pages

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,3 +7,13 @@ class TestWiki:
         response = app.get('/')
         page = response.data.decode('utf-8')
         assert ('wiki' in page and 'lorem' in page)
+
+    def test_page_should_show_page_contents(self, app):
+        response = app.get('/lorem')
+        page = response.data.decode('utf-8')
+        assert '<h1>Lorem ipsum</h1>' in page
+
+    def test_page_should_show_template_on_nonexisting_page(self, app):
+        response = app.get('/Nonexisting')
+        page = response.data.decode('utf-8')
+        assert '<h1>Nonexisting</h1>' in page
--- wiki.py
+++ wiki.py
@@ -1,15 +1,27 @@
 import os
+import codecs
 
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
-app.config.update(DATADIR='tests/data')
+debug = True if os.getenv('FLASK_ENV', 'production') == 'development' else False
+app.config.update(DATADIR='tests/data', DEBUG=debug)
 
 
 @app.route('/', methods=['GET'])
 def index():
     pages = [file[:-5] for file in os.listdir(app.config['DATADIR']) if file.endswith('.html')]
     return render_template('index.html', pages=pages)
+
+
+@app.route('/<page>', methods=['GET'])
+def show_page(page):
+    try:
+        content = codecs.open(os.path.join(app.config['DATADIR'], page + '.html'), 'r', 'utf-8').read()
+        return render_template('page.html', title=page, content=content)
+    except IOError:
+        return render_template('empty.html', title=page)
+
 
 if __name__ == '__main__':
     app.run()
