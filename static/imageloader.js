7f8f1e115969c39a526adf192c6944a445490d0b
Dévai Nándor <mail@devainandor.com>
2014-05-22 19:20:10 +0200

    dropped images are added with inline data

---
+++ static/imageloader.js
@@ -0,0 +1,46 @@
+function handleDrag(event) {
+    event.stopPropagation();
+    event.preventDefault();
+}
+
+function insertFile(file) {
+    var dataURI = file.target.result;
+    var img = document.createElement("img");
+    img.src = dataURI;
+    // Back to 1999…
+    // Firefox
+    if (document.caretPositionFromPoint) {
+        var pos = document.caretPositionFromPoint(x, y);
+        range = document.createRange();
+        range.setStart(pos.offsetNode, pos.offset);
+        range.collapse();
+        range.insertNode(img);
+    }
+    // WebKit
+    else if (document.caretRangeFromPoint) {
+        range = document.caretRangeFromPoint(x, y);
+        range.insertNode(img);
+    } else {
+        // IE…
+    }
+}
+
+function handleDrop(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    x = event.clientX;
+    y = event.clientY;
+    var files = event.dataTransfer.files;
+    for (var i = 0; i < files.length; i++) {
+        file = files.item(i);
+        if (file.type.match('image.*')) {
+            var reader = new FileReader();
+            reader.onload = insertFile;
+            reader.readAsDataURL(file);
+        }
+    }
+}
+
+var dropZone = document.getElementById('content');
+dropZone.addEventListener('dragover', handleDrag, false);
+dropZone.addEventListener('drop', handleDrop, false);
