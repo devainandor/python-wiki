b9cef80799cc7e0969b122729f2d009d63f22f79
Dévai Nándor <mail@devainandor.com>
2014-05-22 19:19:06 +0200

    use POST for new pages, PUT for updates

--- static/app.js
+++ static/app.js
@@ -1,20 +1,23 @@
 var title = document.getElementsByTagName('h1')[0];
 title.contentEditable = true;
-
 var content = document.getElementById('content');
-content.addEventListener('click', function() {
-    content.contentEditable = true;
-});
-content.addEventListener('blur', function() {
-    content.contentEditable = false;
-});
+content.contentEditable = true;
+
+function getMethod() {
+    if (content.getAttribute('data-empty') === 'true') {
+        return 'POST';
+    } else {
+        return 'PUT';
+    }
+}
 
 var saveButton = document.getElementById('save');
 saveButton.addEventListener('click', function() {
     var article = document.getElementsByTagName('article')[0];
     var request = new XMLHttpRequest();
-    request.open('POST', document.URL);
+    request.open(getMethod(), document.URL);
     var formData = new FormData();
     formData.append('content', article.innerHTML);
     request.send(formData);
+    content.setAttribute('data-empty', 'false');
 });
