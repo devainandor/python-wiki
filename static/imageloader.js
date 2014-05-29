5b8344c941bd1cde626627187ba0955d13d147bb
Dévai Nándor <mail@devainandor.com>
2014-05-29 16:03:33 +0200

    JS files organized into modules

--- static/imageloader.js
+++ static/imageloader.js
@@ -1,46 +1,50 @@
-function handleDrag(event) {
-    event.stopPropagation();
-    event.preventDefault();
-}
+var ImageLoader = (function() {
 
-function insertFile(file) {
-    var dataURI = file.target.result;
-    var img = document.createElement("img");
-    img.src = dataURI;
-    var x = event.clientX;
-    var y = event.clientY;
-    // Back to 1999…
-    // Firefox
-    if (document.caretPositionFromPoint) {
-        var pos = document.caretPositionFromPoint(x, y);
-        range = document.createRange();
-        range.setStart(pos.offsetNode, pos.offset);
-        range.collapse();
-        range.insertNode(img);
+    function handleDrag(event) {
+        event.stopPropagation();
+        event.preventDefault();
     }
-    // WebKit
-    else if (document.caretRangeFromPoint) {
-        range = document.caretRangeFromPoint(x, y);
-        range.insertNode(img);
-    } else {
-        // IE…
-    }
-}
 
-function handleDrop(event) {
-    event.stopPropagation();
-    event.preventDefault();
-    var files = event.dataTransfer.files;
-    for (var i = 0; i < files.length; i++) {
-        file = files.item(i);
-        if (file.type.match('image.*')) {
-            var reader = new FileReader();
-            reader.onload = insertFile;
-            reader.readAsDataURL(file);
+    function insertFile(file) {
+        var dataURI = file.target.result;
+        var img = document.createElement("img");
+        img.src = dataURI;
+        var x = event.clientX;
+        var y = event.clientY;
+        // Back to 1999…
+        // Firefox
+        if (document.caretPositionFromPoint) {
+            var pos = document.caretPositionFromPoint(x, y);
+            range = document.createRange();
+            range.setStart(pos.offsetNode, pos.offset);
+            range.collapse();
+            range.insertNode(img);
+        }
+        // WebKit
+        else if (document.caretRangeFromPoint) {
+            range = document.caretRangeFromPoint(x, y);
+            range.insertNode(img);
+        } else {
+            // IE…
         }
     }
-}
 
-var dropZone = document.getElementById('content');
-dropZone.addEventListener('dragover', handleDrag, false);
-dropZone.addEventListener('drop', handleDrop, false);
+    function handleDrop(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        var files = event.dataTransfer.files;
+        for (var i = 0; i < files.length; i++) {
+            file = files.item(i);
+            if (file.type.match('image.*')) {
+                var reader = new FileReader();
+                reader.onload = insertFile;
+                reader.readAsDataURL(file);
+            }
+        }
+    }
+
+    var dropZone = document.getElementById('content');
+    dropZone.addEventListener('dragover', handleDrag, false);
+    dropZone.addEventListener('drop', handleDrop, false);
+
+})();
--- static/search.js
+++ static/search.js
@@ -1,32 +1,36 @@
-function showResults() {
-    clearResults();
-    result = JSON.parse(this.responseText);
-    var filelist = document.getElementById('pages');
-    for (var i=0; i<filelist.children.length; i++) {
-        var href = filelist.children[i].getAttribute('href');
-        if (result.pages.indexOf(href.substring(1)) == -1) {
-            filelist.children[i].style.display = 'none';
+var Search = (function() {
+
+    function showResults() {
+        clearResults();
+        result = JSON.parse(this.responseText);
+        var filelist = document.getElementById('pages');
+        for (var i=0; i<filelist.children.length; i++) {
+            var href = filelist.children[i].getAttribute('href');
+            if (result.pages.indexOf(href.substring(1)) == -1) {
+                filelist.children[i].style.display = 'none';
+            }
         }
     }
-}
 
-function clearResults() {
-    var filelist = document.getElementById('pages');
-    for (var i=0; i<filelist.children.length; i++) {
-        filelist.children[i].style.display = 'block';
+    function clearResults() {
+        var filelist = document.getElementById('pages');
+        for (var i=0; i<filelist.children.length; i++) {
+            filelist.children[i].style.display = 'block';
+        }
     }
-}
 
-function doSearch(event) {
-    if (event.target.value === '') {
-        clearResults();
-        return;
+    function doSearch(event) {
+        if (event.target.value === '') {
+            clearResults();
+            return;
+        }
+        var query = event.target.value;
+        var request = new XMLHttpRequest();
+        request.open('GET', '/search/' + query);
+        request.onload = showResults;
+        request.send();
     }
-    var query = event.target.value;
-    var request = new XMLHttpRequest();
-    request.open('GET', '/search/' + query);
-    request.onload = showResults;
-    request.send();
-}
 
-document.getElementById('search').addEventListener('search', doSearch);
+    document.getElementById('search').addEventListener('search', doSearch);
+    
+})();
