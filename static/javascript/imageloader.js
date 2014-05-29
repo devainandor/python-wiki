0684655cbcc0a4bbb174202abfc0c3f0e69a1288
Dévai Nándor <mail@devainandor.com>
2014-05-29 18:13:44 +0200

    dead code removed

--- static/javascript/imageloader.js
+++ static/javascript/imageloader.js
@@ -7,32 +7,6 @@ CaduceusWiki.ImageLoader = (function() {
         img.src = dataURI;
         var selection = window.getSelection();
         selection.getRangeAt(0).insertNode(img);
-        return;
-        var x = event.clientX;
-        var y = event.clientY;
-        // http://stackoverflow.com/a/10659990
-        var range;
-        if (document.caretPositionFromPoint) {
-            var pos = document.caretPositionFromPoint(x, y);
-            range = document.createRange();
-            range.setStart(pos.offsetNode, pos.offset);
-            range.collapse();
-            range.insertNode(img);
-        }
-        // WebKit
-        else if (document.caretRangeFromPoint) {
-            range = document.caretRangeFromPoint(x, y);
-            range.insertNode(img);
-        }
-        // IE
-        else if (document.body.createTextRange) {
-            range = document.body.createTextRange();
-            range.moveToPoint(x, y);
-            var spanId = "temp_" + ("" + Math.random()).slice(2);
-            range.pasteHTML('<span id="' + spanId + '">&nbsp;</span>');
-            var span = document.getElementById(spanId);
-            span.parentNode.replaceChild(img, span);
-        }
     }
 
     function handleDrop(event) {
